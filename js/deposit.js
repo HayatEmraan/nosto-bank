546f2d2a989c0fc068457cfe4e84ad35c505c2ef
hayatemraan <emraanhasanhayat@gmail.com>
2023-03-08 11:51:59 +0600

    nosto bank

--- js/deposit.js
+++ js/deposit.js
@@ -5,21 +5,19 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
     2. get the value from the element
     3. convert string value to a number
     */
-  const newDepositAmount = getInputFieldValueById("deposit-field");
+  const newDepositAmount = document.getElementById("deposit-field").value;
   /* 
     1. get previous deposit total by id
 
     */
-  const previousDepositTotal = getTextElementValueById("deposit-total");
-
+  const previousDepositTotal = document.getElementById("deposit-total").innerText;
   // calculate new deposit total
-  const newDepositTotal = previousDepositTotal + newDepositAmount;
-  console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
+  const newDepositTotal = parseInt(previousDepositTotal) + parseInt(newDepositAmount);
   // set deposit total value
   setTextElementValueById("deposit-total", newDepositTotal);
 
   // get previous balance by using the function
-  const previousBalanceTotal = getextElementValueById("balance-total");
-  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+  const previousBalanceTotal = document.getElementById("balance-total").innerText;
+  const newBalanceTotal = parseInt(previousBalanceTotal) + parseInt(newDepositAmount);
   setTextElementValueById("balance-total", newBalanceTotal);
-);
+});
